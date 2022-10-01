88f896db81edaa81031c53007d15d24ddc6e08ae
LongHoang <long.hoang.py11@gmail.com>
2022-10-01 20:13:30 +0700

    update package

--- client/src/pages/Registration/index.js
+++ client/src/pages/Registration/index.js
@@ -1,25 +1,11 @@
 import React, { useState } from "react";
 import { Button } from "antd";
 
-import api from "zmp-sdk";
-
 // import QRCode from "react-qr-code";
 
 import "./styles.less";
 
 const Registration = () => {
-  const openZaloCamera = () => {
-    api.viewQr({
-      success: (data) => {
-        // xử lý khi gọi api thành công
-        console.log(data);
-      },
-      fail: (error) => {
-        // xử lý khi gọi api thất bại
-        console.log(error);
-      },
-    });
-  };
   return (
     <>
       <Button
@@ -27,9 +13,8 @@ const Registration = () => {
         htmlType="submit"
         size="large"
         className="login-continue-btn"
-        onClick={openZaloCamera}
       >
-        {"QR"}
+        {"Button"}
       </Button>
     </>
   );
